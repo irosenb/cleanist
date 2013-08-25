33751951396a28e8e3e60961e48f44136ecb4eeb
Isaac Rosenberg <irosenb7@gmail.com>
2013-08-25 15:55:08 -0400

    root page now users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CleanistRails::Application.routes.draw do
   resources :users
 
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
