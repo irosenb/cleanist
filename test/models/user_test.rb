f2a1d2e7eb3fbb3ed5b09c2dc6319681025facd5
Isaac Rosenberg <irosenb7@gmail.com>
2013-11-05 12:03:45 -0500

    trying delayed jobs and whenever

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require "delayed_job_active_record"
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
@@ -30,6 +31,18 @@ class UserTest < ActiveSupport::TestCase
     # update, see if it worked
     @user.update
     created = @user.created_at
+    assert_not_equal created, @user.since
+  end
+
+  test "user can delay job" do
+    created = @user.created_at
+    @user.delay(:run_at => 1.seconds.from_now).update
+    assert_nil @user.since
+
+    sleep(1) 
+
+    puts Delayed::Job.first.inspect 
+    
     assert_not_equal created, @user.since
   end
 
